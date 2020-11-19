974a7993c41b90f2ce7a36c3a256fa1f9838e782
Matthew Bokovitz <MattBokovitz@Matthews-MBP-2.fios-router.home>
2020-11-19 13:12:01 -0500

    revisions

--- my-app/src/components/main-app/AttendeeDetails.js
+++ my-app/src/components/main-app/AttendeeDetails.js
@@ -1,34 +1,34 @@
-import React from "react";
-import { useHistory, Link } from "react-router-dom";
-import axiosWithAuth from "../../utils/axiosWithAuth";
-import { H3, Button, Paragraph } from "../../styles";
+// import React from "react";
+// import { useHistory, Link } from "react-router-dom";
+// import axiosWithAuth from "../../utils/axiosWithAuth";
+// import { H3, Button, Paragraph } from "../../styles";
 
-const AttendeeDetails = ({ attendee }) => {
-  const history = useHistory();
+// const AttendeeDetails = ({ attendee }) => {
+//   const history = useHistory();
 
-  const removeAttendee = (attendee) => {
-    axiosWithAuth()
-      .delete(`/attendees/attendee/${attendee.id}`)
-      .then(() => {
-        history.push("/reload");
-      })
-      .catch((err) => console.log(err));
-  };
-  return (
-    <div className="attendee-details">
-      <H3>{event.name}</H3>
-      <Paragraph>{attendee.name}</Paragraph>
-      <Paragraph>{attendee.foodname}</Paragraph>
-      <Link to={`/edit-attendee/${attendee.id}`} />
-      <Button
-        onClick={() => {
-          removeAttendee(attendee);
-        }}
-      >
-        Delete
-      </Button>
-    </div>
-  );
-};
+//   const removeAttendee = (attendee) => {
+//     axiosWithAuth()
+//       .delete(`/attendees/attendee/${attendee.id}`)
+//       .then(() => {
+//         history.push("/reload");
+//       })
+//       .catch((err) => console.log(err));
+//   };
+//   return (
+//     <div className="attendee-details">
+//       <H3>{event.name}</H3>
+//       <Paragraph>{attendee.name}</Paragraph>
+//       <Paragraph>{attendee.foodname}</Paragraph>
+//       <Link to={`/edit-attendee/${attendee.id}`} />
+//       <Button
+//         onClick={() => {
+//           removeAttendee(attendee);
+//         }}
+//       >
+//         Delete
+//       </Button>
+//     </div>
+//   );
+// };
 
-export default AttendeeDetails;
+// export default AttendeeDetails;
--- my-app/src/components/main-app/MyAttendees.js
+++ my-app/src/components/main-app/MyAttendees.js
@@ -1,37 +1,37 @@
-import React, { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { AppContext } from "./AppContext";
-import axiosWithAuth from "../../utils/axiosWithAuth";
-import { Header, Button } from "../../styles/";
-import AttendeeDetails from "./AttendeeDetails";
+// import React, { useContext, useEffect } from "react";
+// import { Link } from "react-router-dom";
+// import { AppContext } from "./AppContext";
+// import axiosWithAuth from "../../utils/axiosWithAuth";
+// import { Header, Button } from "../../styles/";
+// import AttendeeDetails from "./AttendeeDetails";
 
-const MyAttendees = () => {
-  const [AttendeesList, setAttendeesList] = useContext(AppContext);
+// const MyAttendees = () => {
+//   const [AttendeesList, setAttendeesList] = useContext(AppContext);
 
-  const fetchAttendees = () => {
-    axiosWithAuth()
-      .get("/attendees/attendees")
-      .then((res) => setAttendeesList(res.data))
-      .catch((err) => console.log(err));
-  };
+//   const fetchAttendees = () => {
+//     axiosWithAuth()
+//       .get("/attendees/attendees")
+//       .then((res) => setAttendeesList(res.data))
+//       .catch((err) => console.log(err));
+//   };
 
-  useEffect(() => {
-    fetchAttendees();
-  }, []);
+//   useEffect(() => {
+//     fetchAttendees();
+//   }, []);
 
-  return (
-    <>
-      <div className="event-card">
-        <Header>My Attendees</Header>
-        <Link to={"/add-attendee"}>
-          <Button>New Attendee</Button>
-        </Link>
-        {attendeeList.map((attendee) => {
-          return <AttendeeDetails key={attendee.id} event={attendee} />;
-        })}
-      </div>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <div className="event-card">
+//         <Header>My Attendees</Header>
+//         <Link to={"/add-attendee"}>
+//           <Button>New Attendee</Button>
+//         </Link>
+//         {attendeeList.map((attendee) => {
+//           return <AttendeeDetails key={attendee.id} event={attendee} />;
+//         })}
+//       </div>
+//     </>
+//   );
+// };
 
-export default MyAttendees;
+// export default MyAttendees;
