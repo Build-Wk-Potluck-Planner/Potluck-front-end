189424d475de4872ddcb8b8a074efd9bdc021e77
Maksim Raymond <maksim-raymond@lambdastudents.com>
2020-11-20 11:17:36 -0800

    forms

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -36,3 +36,47 @@ const App = () => {
 };
 
 export default App;
+
+
+// import React, { useState } from "react";
+// import Form from "./Form";
+
+// const defaultValues = {
+//   name: "",
+//   email: "",
+// };
+
+// function App() {
+//   //Values of the form
+//   const [formValues, setFormValues] = useState(defaultValues);
+//   //Storing the values from the form
+//   const [savedFormInfo, setSavedFormInfo] = useState([]);
+//   //The function to handle the onChange inside of the form
+//   const change = (evt) => {
+//     //Turns the evt.target(The place we are interacting with on the form )
+//     //Into something a bit more usable
+//     const { name, value } = evt.target;
+//     //sets the current state of form based off from what is in the form
+//     setFormValues({ ...formValues, [name]: value });
+//   };
+//   //Function for submitting the data to state.
+//   const submit = (evt) => {
+//     //Prevents the default behavious of submit which is reloading the page
+//     evt.preventDefault();
+//     //packaging an easy to use payload to put onto state
+//     const newData = {
+//       name: formValues.name.trim(),
+//       email: formValues.email.trimEnd(),
+//     };
+//     //adding the data to state
+//     setSavedFormInfo([...savedFormInfo, newData]);
+//     setFormValues(defaultValues);
+//   };
+//   return (
+//     <div className="App">
+//       <Form formValues={formValues} change={change} submit={submit}/>
+//     </div>
+//   );
+// }
+
+// export default App;
